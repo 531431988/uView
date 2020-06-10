54e4b5f411d1b2fadbbe1d3d65038f71fd3519dd
wm <531431988@qq.com>
2020-06-10 16:36:26 +0800

    添加下拉刷新

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -9,12 +9,20 @@
       }"
     >
       <view class="u-flex u-padding-24" slot="right">
-        <view class="u-m-r-24" style="color: #fff">编辑</view>
-        <u-icon name="more-dot-fill" color="#ffffff" size="38"></u-icon>
+        <view class="u-m-r-24" style="color: #fff" @click="onEdit">{{ isEdit ? '完成' : '编辑' }}</view>
+        <!-- <u-icon name="more-dot-fill" color="#ffffff" size="38"></u-icon> -->
       </view>
     </u-navbar>
     <view class="cart">
-      <scroll-view scroll-y style="height: 100%;width: 100%;">
+      <scroll-view
+        scroll-y
+        style="height: 100%;width: 100%;"
+        :refresher-threshold="80"
+        :refresher-enabled="enabled"
+        :refresher-triggered="triggered"
+        @refresherrefresh="onRefresh"
+        v-if="cartList.length"
+      >
         <u-swipe-action
           bg-color="transparent"
           :show="item.show"
@@ -26,7 +34,13 @@
           :key="item.id"
         >
           <view class="card u-flex u-m-b-24">
-            <u-icon :name="item.checked ? 'checkbox-fill' : 'checkbox'" custom-prefix="icon" size="48" :color="item.checked ? '#fa3534' : '#909399'" @click="onCheck(index)"></u-icon>
+            <u-icon
+              :name="item.checked ? 'checkbox-fill' : 'checkbox'"
+              custom-prefix="icon"
+              size="48"
+              :color="item.checked ? '#fa3534' : '#909399'"
+              @click="onCheck(index)"
+            ></u-icon>
             <view class="u-m-l-24 u-m-r-24" @click="onOpenPage(item.id)"><image :src="item.url" mode="aspectFill"></image></view>
             <view>
               <view class="title u-line-2" @click="onOpenPage(item.id)">{{ item.title }}</view>
@@ -45,89 +59,34 @@
                 />
               </view>
               <view class="u-flex u-row-between">
-                <view class="price">
-                  ￥ {{item.price}}
-                </view>
+                <view class="price">￥ {{ item.price }}</view>
                 <view class="number"><u-number-box :value="item.number" @change="onNumChange($event, index)"></u-number-box></view>
               </view>
             </view>
           </view>
         </u-swipe-action>
       </scroll-view>
-      <u-popup v-model="show" mode="bottom">
-        <view class="u-p-24">
-          <view style="min-height: 50vh;">
-            <view class="u-flex">
-              <image :src="goodsParams.src"></image>
-              <view class="price u-m-l-24">￥ {{ goodsParams.price }}</view>
-            </view>
-            <view>
-              <view class="u-m-b-24 u-m-t-24">颜色</view>
-              <view>
-                <u-tag
-                  v-for="(item, index) in goodsParams.color"
-                  :key="index"
-                  :text="item.text"
-                  :type="item.checked ? 'warning' : 'info'"
-                  :mode="item.checked ? 'dark' : 'light'"
-                  shape="circle"
-                  class="u-m-r-16"
-                  :closeable="false"
-                />
-              </view>
-              <view class="u-m-b-24  u-m-t-24">尺寸</view>
-              <view>
-                <u-tag
-                  v-for="(item, index) in goodsParams.size"
-                  :key="index"
-                  :text="item.text"
-                  :type="item.checked ? 'warning' : 'info'"
-                  :mode="item.checked ? 'dark' : 'light'"
-                  shape="circle"
-                  class="u-m-r-16"
-                  :closeable="false"
-                />
-              </view>
-              <view class="u-m-b-24  u-m-t-24">服务</view>
-              <view>
-                <u-tag
-                  v-for="(item, index) in goodsParams.server"
-                  :key="index"
-                  :text="item.text"
-                  :type="item.checked ? 'warning' : 'info'"
-                  :mode="item.checked ? 'dark' : 'light'"
-                  shape="circle"
-                  class="u-m-r-16"
-                  :closeable="false"
-                />
-              </view>
-            </view>
-          </view>
-          <u-button type="warning">确定</u-button>
-        </view>
-      </u-popup>
+      <u-empty v-else text="购物车为空" mode="car"></u-empty>
     </view>
 
-    <view class="u-flex u-row-between navigation u-p-l-24 u-p-r-24 u-p-t-16 u-p-b-16">
-      <view class=" u-flex-1">
-        <view class="u-flex u-col-center u-row-between">
-          <view class="u-flex u-col-center" @click="onCheckAll">
-            <u-icon :name="checkAll ? 'checkbox-fill' : 'checkbox'" custom-prefix="icon" size="48" :color="checkAll ? '#fa3534' : '#909399'"></u-icon>
-            <view class="u-m-l-8">全选</view>
-          </view>
-          <view class="u-m-r-24">合计：<text class="price u-m-r-5">{{total}}</text>元</view>
-        </view>
-      </view>
-      <u-button type="error" size="medium">去结算 ({{goodsNumber}})</u-button>
-    </view>
+    <u-popup v-model="show" mode="bottom"><ParamsPopup :data="goodsParams" /></u-popup>
+    <SubmitBar v-if="cartList.length" v-model="checkAll" :edit="isEdit" :total="total" :number="goodsNumber" @click="onCheckAll" @on-del="onDelMultiple" />
   </view>
 </template>
 
 <script>
-  import {numAdd} from '@/common/libs.js'
+import { numAdd } from '@/common/libs.js';
+import ParamsPopup from './components/ParamsPopup';
+import SubmitBar from './components/SubmitBar';
 export default {
+  components: {
+    ParamsPopup,
+    SubmitBar
+  },
   data() {
     return {
+      enabled: true,
+      triggered: false,
       cartList: [],
       options: [
         {
@@ -137,42 +96,66 @@ export default {
           }
         }
       ],
-      goodsParams: {},
+      isEdit: false,
       show: false,
       checkAll: false,
+      goodsParams: {},
       goodsNumber: 0,
       total: 0
     };
   },
-  onLoad() {
-    this.$u.post('cart/list').then(res => {
-      this.cartList = res.map(item => ({
-        ...item,
-        show: false,
-        checked: false
-      }));
-    });
-  },
-  computed: {
+  onShow() {
+    this.onLoadData();
+    this.isEdit = false;
   },
   methods: {
-    // 删除商品
+    // 加载数据
+    onLoadData() {
+      if (!this.enabled) return;
+      this.triggered = true;
+      this.$u.post('cart/list').then(res => {
+        this.cartList = res.map(item => ({
+          ...item,
+          show: false,
+          checked: false
+        }));
+        this.triggered = false;
+      });
+    },
+    // 编辑
+    onEdit() {
+      this.cartList = this.cartList.map(item => ({
+        ...item,
+        checked: false
+      }));
+      this.checkAll = false;
+      this.total = 0;
+      this.goodsNumber = 0;
+      this.isEdit = !this.isEdit;
+      this.enabled = !this.isEdit;
+    },
+    // 删除多条商品
+    onDelMultiple() {
+      this.cartList = this.cartList.filter(item => !item.checked);
+      this.checkAll = false;
+      this.updateNumberMoney();
+    },
+    // 滑动删除商品
     onDel(index) {
       this.cartList.splice(index, 1);
       this.$u.toast('删除成功');
     },
     // 数量改变
-    onNumChange (e, i) {
-      this.cartList[i].number = e.value
-      this.updateNumberMoney()
+    onNumChange(e, i) {
+      this.cartList[i].number = e.value;
+      this.updateNumberMoney();
     },
     // 单选
-    onCheck (index) {
-      this.cartList[index].checked = !this.cartList[index].checked
-      let checklist = this.cartList.filter(item => item.checked === true)
-      this.checkAll = checklist.length === this.cartList.length
-      this.updateNumberMoney()
-      
+    onCheck(index) {
+      this.cartList[index].checked = !this.cartList[index].checked;
+      let checklist = this.cartList.filter(item => item.checked);
+      this.checkAll = checklist.length === this.cartList.length;
+      this.updateNumberMoney();
     },
     // 全选
     onCheckAll() {
@@ -181,17 +164,17 @@ export default {
         ...item,
         checked: this.checkAll
       }));
-      let checklist = this.cartList.filter(item => item.checked === true)
-      this.updateNumberMoney()
+      let checklist = this.cartList.filter(item => item.checked);
+      this.updateNumberMoney();
     },
     // 更新数量与金额
-    updateNumberMoney () {
-      let checklist = this.cartList.filter(item => item.checked === true)
-      this.total = 0
-      this.goodsNumber = checklist.length
+    updateNumberMoney() {
+      let checklist = this.cartList.filter(item => item.checked);
+      this.total = 0;
+      this.goodsNumber = checklist.length;
       checklist.forEach(item => {
-        this.total = numAdd(this.total, item.price*item.number) 
-      })
+        this.total = numAdd(this.total, item.price * item.number);
+      });
     },
     // 如果打开一个的时候，不需要关闭其他，则无需实现本方法
     onSwipeOpen(index) {
@@ -215,21 +198,8 @@ export default {
         url: '/pages/cart/detail'
       });
     },
-    // 总价
-    totalPrice(item) {
-      let price = 0;
-      item.map(val => {
-        price += parseFloat(val.price);
-      });
-      return price.toFixed(2);
-    },
-    // 总件数
-    totalNum(item) {
-      let num = 0;
-      item.map(val => {
-        num += val.number;
-      });
-      return num;
+    onRefresh() {
+      this.onLoadData();
     }
   }
 };
@@ -277,13 +247,7 @@ page {
     border-radius: 10rpx;
   }
 }
-  .price {
-    color: $u-type-warning;
-  }
-.navigation {
-  background-color: #ffffff;
-  .u-btn {
-    padding: 0 18px;
-  }
+.price {
+  color: $u-type-warning;
 }
 </style>
